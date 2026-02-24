2a7a429a00afebb4416bbf40315c87b038628b10
K AKHIL KUMAR <140427573+Akhilkumar24@users.noreply.github.com>
2026-02-24 14:33:51 +0530

    Update index.js

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,1 +1,16 @@
 
+import { Router } from "express";
+
+import ideasRoutes from "./ideas.routes";
+import experimentsRoutes from "./experiments.routes";
+import outcomesRoutes from "./outcomes.routes";
+import reflectionsRoutes from "./reflections.routes";
+
+const router = Router();
+
+router.use("/ideas", ideasRoutes);
+router.use("/experiments", experimentsRoutes);
+router.use("/outcomes", outcomesRoutes);
+router.use("/reflections", reflectionsRoutes);
+
+export default router;
