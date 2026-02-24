2bef9c955b71bd0d69c0df3747d9627ffabdbe6d
K AKHIL KUMAR <140427573+Akhilkumar24@users.noreply.github.com>
2026-02-24 14:39:29 +0530

    created the versioned router
    i have created the versioned router in the new file called the index.ts in the v1

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,16 +1,19 @@
-
 import { Router } from "express";
 
-import ideasRoutes from "./ideas.routes";
-import experimentsRoutes from "./experiments.routes";
-import outcomesRoutes from "./outcomes.routes";
-import reflectionsRoutes from "./reflections.routes";
+import ideasRoutes from "../ideas.routes";
+import experimentsRoutes from "../experiments.routes";
+import outcomesRoutes from "../outcomes.routes";
+import reflectionsRoutes from "../reflections.routes";
+import authRoutes from "../auth.routes";
+import insightsRoutes from "../insights.routes";
 
 const router = Router();
 
+router.use("/auth", authRoutes);
 router.use("/ideas", ideasRoutes);
 router.use("/experiments", experimentsRoutes);
 router.use("/outcomes", outcomesRoutes);
 router.use("/reflections", reflectionsRoutes);
+router.use("/insights", insightsRoutes);
 
 export default router;
